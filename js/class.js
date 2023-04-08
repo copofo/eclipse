8908abb7ec67a2698d92489a5d0e97dec7c5457a
copofo <copofo4243@gmail.com>
2023-04-07 23:21:10 -0300

    9

--- js/class.js
+++ js/class.js
@@ -23,6 +23,28 @@ class Sprite{
 }
 
 
+class Projectile extends Sprite{
+  
+  
+  constructor(x,y,radius,color,velocity){
+    
+    super(x,y,radius,color)
+    this.velocity = velocity
+    
+    
+    
+  }
+  
+  update(){
+    this.draw()
+    this.x += this.velocity.x
+    this.y += this.velocity.y
+  }
+  
+  
+}
+
+
 
 class Sphere extends Sprite{
   
--- js/src.js
+++ js/src.js
@@ -18,6 +18,33 @@ const ctx = cnv.getContext('2d')
 
 const player = new Player(cnv.width/2,cnv.height/2,20,'#48fcff')
 
+let projectiles = []
+
+const shootingSpeed = 4
+
+
+
+
+
+cnv.addEventListener('click',(e)=>{
+  
+  e.preventDefault()
+  const angle = Math.atan2(e.clientY - player.y, e.clientX - player.x )
+  
+  const velocity = {
+    
+    x: Math.cos(angle) * shootingSpeed,
+    y: Math.sin(angle) * shootingSpeed
+  }
+  projectiles.push(new Projectile(player.x,player.y,3,"#48fcff",velocity))
+})
+
+
+
+
+
+
+
 
 function loop(){
   
@@ -31,10 +58,25 @@ function update(){
   ctx.fillStyle = "rgba(0,0,0,0.1)"
   ctx.fillRect(0,0,cnv.width,cnv.height)
   
+  
+  checkProjectiles()
+  
   player.update()
   
 }
 
+function checkProjectiles(){
+  
+  for(let i = projectiles.length -1; i >= 0; i--){
+    
+    const p = projectiles[i]
+    p.update()
+    
+  }
+  
+  
+  
+}
 
 
 loop()
