e39de9b4b8391458f23694e4f4a83dbe9c37d777
copofo <copofo4243@gmail.com>
2023-04-07 22:28:28 -0300

    8

--- js/src.js
+++ js/src.js
@@ -16,7 +16,7 @@ cnv.height = innerHeight
 
 const ctx = cnv.getContext('2d')
 
-const player = new Sprite(cnv.width/2,cnv.height/2,20,'white')
+const player = new Player(cnv.width/2,cnv.height/2,20,'#48fcff')
 
 
 function loop(){
@@ -31,7 +31,7 @@ function update(){
   ctx.fillStyle = "rgba(0,0,0,1)"
   ctx.fillRect(0,0,cnv.width,cnv.height)
   
-  player.draw()
+  player.update()
   
 }
 
